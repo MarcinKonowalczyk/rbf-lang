5a95f6bb41a1d17e2a7f1ccd0e41a699c020167c
Marcin <marcin.konow@lczyk.xyz>
2024-03-09 13:54:08 +0000

    reversibility

--- rbf_lang/__init__.py
+++ rbf_lang/__init__.py
@@ -2,12 +2,14 @@
 Reversible BitFuck (RBF) interpretor
 """
 
-__version__ = "0.1.0.post2"
+__version__ = "0.2.0"
 
 from . import program
 from . import tape
 from . import runner
+from . import reverse
 
 Program = program.Program
 Tape = tape.Tape
 run = runner.run
+reverse_program = reverse.reverse_program
